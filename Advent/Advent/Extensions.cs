363f59378ff1aacbc697c56586dbc54255b8457c
Peter Gillett <peter_gillett@outlook.com>
2024-12-22 11:47:11 +0000

    Fixed the line splits (difference between newline in test and download)

--- Advent/Advent/Extensions.cs
+++ Advent/Advent/Extensions.cs
@@ -5,8 +5,8 @@ namespace Advent;
 public static class Extensions
 {
     public static string[] IntoLines(this string str) =>
-        str.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        str.Split(["\n", "\r\n"], StringSplitOptions.RemoveEmptyEntries);
 
     public static string[] IntoSections(this string str) =>
-        str.Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+        str.Split(["\n\n", "\r\n\r\n"], StringSplitOptions.RemoveEmptyEntries);
 }
